667f0ab1c7315f06e5151d99e3082e48af16b081
Hashir Ahsan <93591488+Hash650@users.noreply.github.com>
2025-03-15 10:56:42 -0500

    Update PrivateRoutes.jsx
    debug

--- client/src/utils/PrivateRoutes.jsx
+++ client/src/utils/PrivateRoutes.jsx
@@ -7,6 +7,7 @@ const PrivateRoute = ({ children }) => {
     const { isAuthenticated } = useContext(AuthContext)
     const location = useLocation()
 
+    console.log(location.pathname);
     if (location.pathname === '/verification-success') {
         return children; // Allow access to /verification-success without auth
     }
