f17891db0d42be309387c097a473e8395eba3fda
Hashir Ahsan <93591488+Hash650@users.noreply.github.com>
2025-03-15 11:26:07 -0500

    Update PrivateRoutes.jsx
    roll back hidden route fix

--- client/src/utils/PrivateRoutes.jsx
+++ client/src/utils/PrivateRoutes.jsx
@@ -1,16 +1,10 @@
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
-import { useContext, useLocation } from "react";
+import { useContext } from "react";
 
 const PrivateRoute = ({ children }) => {
 
     const { isAuthenticated } = useContext(AuthContext)
-    const location = useLocation()
-
-    console.log(location.pathname);
-    if (location.pathname === '/verification-success') {
-        return children; // Allow access to /verification-success without auth
-    }
     // console.log(isAuthenticated)
     return isAuthenticated ? children : <Navigate to='/login' />
 };
